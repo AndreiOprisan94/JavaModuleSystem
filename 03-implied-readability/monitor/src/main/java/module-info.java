a4d6a02d9a06ffd49e7053fcdb5b39eea4291dac
oprisan <aoprisan@adobe.com>
2018-05-29 14:59:08 +0300

    [Third exercise] Third task, using aggregator module

--- 03-implied-readability/monitor/src/main/java/module-info.java
+++ 03-implied-readability/monitor/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module monitor {
 //	requires monitor.observer;
-	requires monitor.observer.alpha;
+//	requires monitor.observer.alpha;
 	// requires monitor.observer.beta;
 //	requires monitor.statistics;
+	requires monitor.observation;
 	requires monitor.persistence;
 	requires monitor.rest;
 }
